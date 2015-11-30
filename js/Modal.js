0b16daaeaefa6e57ad541964488fc88d3587a25f
tvixxx <tvix2000@yandex.ru>
2015-11-30 23:02:20 +0400

    Added check for our login and password

--- js/Modal.js
+++ js/Modal.js
@@ -1,15 +1,32 @@
+
 var link = document.querySelector(".login");
 var popup = document.querySelector(".modal-content");
-var close = document.querySelector(".modal-content-close");
-var login = document.querySelector("[name=login");
 
-link.addEventListener("click", function(event){
-	event.preventDefault();
-	popup.classList.add("show");
-	login.focus;
-});
+var close = popup.querySelector(".modal-content-close");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var form = popup.querySelector("form");
 
-close.addEventListener("click", function(event){
-	event.preventDefault();
-	popup.classList.remove("show");
-});
+	link.addEventListener("click", function(event){
+		event.preventDefault();
+		popup.classList.add("show");
+		login.focus;
+	});
+
+		close.addEventListener("click", function(event){
+			event.preventDefault();
+			popup.classList.remove("show");
+		});
+
+			form.addEventListener("submit", function (event){
+				if (!(login.value && password.value)){
+					event.preventDefault();
+					alert("Nope");
+				} else if (!(login.length < 4)) {
+					event.preventDefault();
+					alert ("low symbols in login!")
+				} else if (!(password.length < 6)){
+					event.preventDefault();
+					alert("low symbols in password");
+				};
+			});
